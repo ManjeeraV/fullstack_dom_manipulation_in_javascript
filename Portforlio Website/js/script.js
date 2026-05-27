2e40417116824a34e318c2ad67f4cb8b639ca53d
ManjeeraV <reachmanjeerav@gmail.com>
2026-05-28 00:47:56 +0530

    DOM Manipuation in JS Portfolio Website

---
+++ Portforlio Website/js/script.js
@@ -0,0 +1,60 @@
+// This function should retrieve all the project objects from projects array.
+// It should then traverse over the array to create individual cards displaying each project details.
+function loadProjects() {
+  // Write your code here
+  var textHTML = '';
+  for(var i=0;i<projects.length;i++){
+    textHTML = textHTML + '<div class="card"><img id="img" src='+projects[i].image+'> ' +
+    '<span>' + projects[i].title + '</span><span>' + projects[i].description + '</span></div>';
+  }
+  document.getElementById("projects").innerHTML = textHTML;
+
+  //Additional logic to overwrite the About Me content using DOM 
+  
+  //below select the first tag element with section class
+  const divElement = document.querySelector('.section');
+  const paraElement= divElement.querySelector('p');
+  paraElement.style.textAlign='center';
+  paraElement.textContent = "Welcome to my Portfolio Website. I am learning full stack development program via Great Learning platform";
+}
+
+// This function should return the projectId of the new project
+function newProjectId(){
+  // Write code to create and return new Project Id
+  var currentProjectCount= 0;
+  currentProjectCount = projects.length;
+  currentProjectCount++
+  return currentProjectCount;
+}
+
+function saveNewProject() {
+
+  // Write your code here
+  // Get the new project details by using the DOM elements
+  var newTitle = document.getElementById("title").value;
+  var newDesc= document.getElementById("desc").value;
+  var newImage= document.getElementById("image").value;
+
+  // Create the new projectId by calling the newProjectId() function
+  var newProjId = newProjectId();
+
+  // Create a new project object
+  var newProjectObj = {
+    "id":newProjId,
+    "title":newTitle,
+    "description":newDesc,
+    "image":newImage
+  };
+  projects.push(newProjectObj);
+  // Add the new project object to the projects array 
+  
+
+  // Load the projects after adding the new project
+  loadProjects();
+
+  // Clear the values of the New Project Details Form after adding the new project
+  document.getElementById("title").value = "";
+  document.getElementById("desc").value = "";
+  document.getElementById("image").value = "";
+}
+loadProjects();
---
+++ readme.md
@@ -0,0 +1,19 @@
+The Portfolio Website is created using HTML, CSS and JavaScript. The concepts of DOM manipulations are used in this project.
+
+
+The details of the project are as follows :
+● The website contains a single page.
+
+● The web page has three sections:
+ o About Me: Here the website owners will mention information about themselves.  
+ o Enter New Project Details: Here they can enter details about the new project they want to add to their portfolio. 
+ Once they click on the save button, the project will be added to the existing projects on the webpage.
+ o My Projects: Here they can display their projects with details like project title, description and image.
+ 
+● You will find the following files in the folder: 
+ o index.html: This file contains the html for the webpage.
+ o styles.css: This file contains the styling for index.html.
+ o projects.js: This file contains an array of projects.The array consists of a collection of objects. Each object consists of details regarding a project such as title, description and image.
+ o script.js: In this file, you have to add code to complete the tasks which are mentioned in the Problem Statement.
+
+ To run the application on your default browser, double click the index.html file.
